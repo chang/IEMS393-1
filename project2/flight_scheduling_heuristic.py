c2ca49525bb7c4e35ff2ea8e86746baef611ecfd
Eric Chang <eric@Erics-MacBook-Pro.local>
2017-02-16 03:09:18 -0600

    update heuristic

--- project2/flight_scheduling_heuristic.py
+++ project2/flight_scheduling_heuristic.py
@@ -5,34 +5,16 @@ Darren, Eric, Jonathan, Katie
 """
 import pandas as pd
 
-def is_feasible_connection(flight1, flight2, fleet, flight_data):
-    """
-    takes two flight ids and a potential fleet and returns True if:
-    1. flight1's destination connects with flight2's origin
-    2. the time gap between flight1 and flight2 is less than the plane's turn time
-    3. ??? block time??
-    4. ??? minimum time ???
-    """
-    TURNTIME = 30/(60*24)
-    flight1_dest = flight_data[flight_data['flight'] == flight1].reset_index().at[0, 'dest']
-    flight2_orig = flight_data[flight_data['flight'] == flight2].reset_index().at[0, 'origin']
-    
-    flight1_arrtime = flight_data[flight_data['flight'] == flight1].reset_index().at[0, 'arr_time']
-    flight2_deptime = flight_data[flight_data['flight'] == flight2].reset_index().at[0, 'dep_time']
-    time_diff = flight2_deptime - flight1_arrtime
 
-    # consider adding a max turn time as well, to avoid too much waiting on the ground
-    return (flight1_dest == flight2_orig) and (time_diff >= TURNTIME)
-
-
-def get_turntime(flight1, flight2, flight_data):
-    """
-    takes two flight ids and a potential fleet and returns turntime
-    """
-    flight1_arrtime = flight_data[flight_data['flight'] == flight1].reset_index().at[0, 'arr_time']
-    flight2_deptime = flight_data[flight_data['flight'] == flight2].reset_index().at[0, 'dep_time']
-    time_diff = flight2_deptime - flight1_arrtime
-    return time_diff
+def create_tail_numbers():
+	"""returns list of unique tail numbers for the 5 fleets"""
+	tail_numbers =  \
+		['a' + str(i) for i in range(101)] +  \
+		['b' + str(i) for i in range(57)] +  \
+		['c' + str(i) for i in range(97)] +  \
+		['d' + str(i) for i in range(78)] +  \
+		['e' + str(i) for i in range(117)]
+	return tail_numbers
 
 
 def get_fleet_order(info):
@@ -45,15 +27,36 @@ def get_fleet_order(info):
 	return fleet_order
 
 
-def create_tail_numbers():
-	"""returns list of unique tail numbers for the 5 fleets"""
-	tail_numbers =  \
-		['a' + str(i) for i in range(101)] +  \
-		['b' + str(i) for i in range(57)] +  \
-		['c' + str(i) for i in range(97)] +  \
-		['d' + str(i) for i in range(78)] +  \
-		['e' + str(i) for i in range(117)]
-	return tail_numbers
+def get_turntime(flight1, flight2, flight_data):
+    """
+    takes two flight ids and a potential fleet and returns turntime
+    """
+    flight1_arrtime = flight_data[flight_data['flight'] == flight1].reset_index().at[0, 'arr_time']
+    flight2_deptime = flight_data[flight_data['flight'] == flight2].reset_index().at[0, 'dep_time']
+    time_diff = flight2_deptime - flight1_arrtime
+    return time_diff
+
+
+def is_feasible_connection(flight1, flight2, fleet, flight_data):
+    """
+    takes two flight ids and a potential fleet and returns True if:
+    1. flight1's destination connects with flight2's origin
+    2. the time gap between flight1 and flight2 is less than the plane's turn time
+    3. ??? block time??
+    4. ??? minimum time ???
+    """
+    TURNTIME = 30/(60*24)
+
+    flight1_dest = flight_data[flight_data['flight'] == flight1].reset_index().at[0, 'dest']
+    flight2_orig = flight_data[flight_data['flight'] == flight2].reset_index().at[0, 'origin']
+    flight1_arrtime = flight_data[flight_data['flight'] == flight1].reset_index().at[0, 'arr_time']
+    flight2_deptime = flight_data[flight_data['flight'] == flight2].reset_index().at[0, 'dep_time']
+
+    time_diff = flight2_deptime - flight1_arrtime
+
+    # consider adding a max turn time as well, to avoid too much waiting on the ground
+    return (flight1_dest == flight2_orig) and (time_diff >= TURNTIME)
+
 
 def read_ampl_data():
 	"""
@@ -74,7 +77,7 @@ def assign_flights():
 
 	an improvement could be to sort flights by mean or max profitability, so they are filled optimally first
 	"""
-	flight_data = pd.read_csv("data/final_flight_data_truncated.csv")
+	flight_data = pd.read_csv("data/final_flight_data.csv")
 	flight_data.loc[:,['a', 'b', 'c', 'd', 'e']] = flight_data.loc[:,['a', 'b', 'c', 'd', 'e']].apply(pd.to_numeric)
 	flight_list = list(flight_data['flight'])
 	assignments = dict((key, []) for key in create_tail_numbers())
@@ -82,6 +85,7 @@ def assign_flights():
 	nflights = len(flight_list)
 	count_assigned = 0
 	profit = 0
+	count_skipped = 0
 	turn_times = []
 
 	while flight_list:
@@ -104,8 +108,12 @@ def assign_flights():
 					# assign the plane to the flight if it is a viable connecting flight or it's an empty plane
 					if connecting_flight or not assignments[tail_number]:
 						if connecting_flight:
-							turn_times.append(get_turntime(flight1=assignments[tail_number][-1], flight2=flight, flight_data=flight_data))
+							turn_times.append(get_turntime(flight1=assignments[tail_number][-1], 
+														   flight2=flight, 
+														   flight_data=flight_data))
 						assignments[tail_number].append(flight)
+
+						print("Output Schedule:", assignments[tail_number])
 						profit += info.reset_index().at[0, best_fleet]
 						print(profit)						
 						is_assigned = True
@@ -114,9 +122,8 @@ def assign_flights():
 
 			# raise an error if gone through all fleets, otherwise move to next best fleet
 			if not is_assigned and not fleet_order:
-				print("RAN OUT OF FEASIBLE FLIGHTS")
-				return (assignments, turn_times, profit)
-				raise ValueError("There are no more feasible tail numbers.")
+				count_skipped += 1
+				print("Skipping", str(count_skipped))
 			elif not is_assigned:
 				best_fleet = fleet_order.pop()
 
@@ -130,15 +137,15 @@ if __name__ == "__main__":
 	(t, turn_times, profit) = assign_flights()
 
 	# write flight assignments to tab delimited file
-	out = open("flight_assignments.txt", "w")
+	out_assignments = open("flight_assignments.txt", "w")
 	out_turntimes = open("turntimes.txt", "w")
 	out_profit = open("profit.txt", "w")
 
 	for tail in t.keys():
-		out.write(tail)
+		out_assignments.write(tail)
 		for flight in t[tail]:
-			out.write("\t" + str(flight))
-		out.write("\n")
+			out_assignments.write("\t" + str(flight))
+		out_assignments.write("\n")
 
 	for time in turn_times:
 		out_turntimes.write(str(time) + "\n")
